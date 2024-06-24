8f03a3216c0c4d14428bea1c5f8ef476c61e55d6
Luana-Nascimento <luananascimento00@outlook.com>
2024-06-24 11:39:35 -0300

    Hook useState em React com TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
 import Home from './pages/home/Home'
 import './App.css'
+import React, {useState} from 'react'
 
 function App() {
+  const [contador, setContador] = useState(1)
+  function somar(){
+    setContador(contador + 1)
+  }
   return (
-    <Home/>
-  )
+
+    <div>
+    <h1> App</h1>
+    <p> O valor é: {contador} </p> 
+    <button onClick={somar}>Adicionar + 1</button>
+    </div>
+    )
 }
 
 export default App
+
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
-import './Home.css'
+import imagem from '../../assets/html-css-collage-concept.jpg'
 
-function Home(){
+interface myProps{
+  titulo: string,
+  texto: string,
+}
+
+function Home(props: myProps){
+
+  /*const [state (variável), setState] = useState(initialState);*/
+  
+  /*espera um motivo para causar um efeito,ex: efeito do botão de comprar > colocar o produto no carrinho, ao acionar 2x a senha, posso comparar ambas para ver se está correta 
+    useEffect(() => {
+      alert('para de somar')}, [contador])
+
+    const [completed, setCompleted] = useState(false);
+    const [tarefa, setTarefa] = useState('Clique no botão para finalizar a tarefa');
+
+    /*cookbook
+    useEffect(() => {
+      if (completed) {
+        setTarefa('Parabéns! Você concluiu a tarefa!');
+      }
+    }, [completed]);*/
+
   return (
     <>
-        <h1 className='titulo'>Home</h1>
-        <img src='https://img.freepik.com/fotos-gratis/conceito-de-colagem-de-html-e-css_23-2150061955.jpg?t=st=1719191306~exp=1719194906~hmac=56458298caba3ac35dc6670590369945639ed0c8a2005e43ff0ced1279e426a8&w=996' className='img' alt="Imagem Dev" />
+        {/*<h1 style={{fontSize: '24px'}}>{props.titulo} </h1>
+        <p> {props.texto}</p>
+        <img src={imagem} alt="" height={150} /> */}
+   
+   {/* <h1>{contador}</h1>
+    <button onClick={somar}>Adicionar + 1</button>
+
+    <h3>{tarefa}</h3>
+      <button onClick={() => setCompleted(true)}>Concluir Tarefa</button> */}
+    
+
+    {/*if ternário}
+
+    {logado == false ? <button onClick={() => {setLogado(true)}}>Cloque aqui para logar</button> 
+    : <h1>Parabéns você está logado</h1> */}
+    
     </>
   )
 }
