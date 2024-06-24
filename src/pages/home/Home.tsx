ae30215c429a8b25daa647babc17d15d13e5d022
Luana-Nascimento <luananascimento00@outlook.com>
2024-06-23 22:59:47 -0300

    Aplicando estilização com React

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+function Home(){
+  return (
+    <>
+        <h1 className='titulo'>Home</h1>
+        <img src='https://img.freepik.com/fotos-gratis/conceito-de-colagem-de-html-e-css_23-2150061955.jpg?t=st=1719191306~exp=1719194906~hmac=56458298caba3ac35dc6670590369945639ed0c8a2005e43ff0ced1279e426a8&w=996' className='img' alt="Imagem Dev" />
+    </>
+  )
+}
+
+export default Home
+
