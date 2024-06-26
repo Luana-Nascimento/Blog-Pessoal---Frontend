44a7f8f68738a84c0b9442f4e7884eb9a1a962b8
Luana-Nascimento <luananascimento00@outlook.com>
2024-06-26 09:27:05 -0300

    adicionando sistema de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import Home from './pages/home/Home'
-import './App.css'
-import React, {useState} from 'react'
+import './App.css';
+import Home from './pages/home/Home';
+import Navbar from './componentes/navbar/navbar';
+import Footer from './componentes/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
 
 function App() {
-
-  return(
-
-    <div> 
-      <Home/> 
-    </div>
-    )
+  return (
+    <>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+);
 }
-
-export default App
+export default App;
 
 
